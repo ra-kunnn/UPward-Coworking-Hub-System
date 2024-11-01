fe3f789e9b9c12122b529bd6df5f69f10864d3dd
centuriee <117663072+centuriee@users.noreply.github.com>
2024-11-01 20:01:56 +0800

    rough outline

--- upwardApp/src/lib/landing/headerLanding.svelte
+++ upwardApp/src/lib/landing/headerLanding.svelte
@@ -21,12 +21,12 @@
     }
 </script>
 
-<div class="sticky top-0 z-40 w-full p-5 mx-auto sm:px-8 xl:px-12 bg-white shadow-xl">
-    <div class="flex justify-between mx-auto items-center">
-        <span class="font-semibold max-sm:font-bold text-xl max-md:text-base max-[480px]:text-xs">UPAD Management System</span>
-        <div class="flex items-center gap-1">
-            <button on:click={logIn} class="bg-inherit hover:variant-soft-primary rounded-none border-none px-5 max-md:px-2 py-2 max-md:text-sm max-[480px]:text-xs font-semibold">Log In</button>
-            <button on:click={signUp} class="bg-inherit hover:variant-soft-primary rounded-none border-none px-5 max-md:px-2 py-2 max-md:text-sm max-[480px]:text-xs font-semibold">Sign Up</button>
+<div class="sticky top-0 z-40 h-20 w-full p-5 mx-auto bg-secondary-800 shadow-xl">
+    <div class="flex justify-between mx-20 items-center text-tertiary-300">
+        <span class="font-semibold max-sm:font-bold text-xl max-md:text-base max-[480px]:text-xs">UPward</span>
+        <div class="flex items-center gap-3">
+            <button on:click={logIn} class="bg-surface-50 rounded border-none px-3 py-1 my-1 text-sm font-semibold">Log In</button>
+            <button on:click={signUp} class="bg-tertiary-500 text-surface-50 rounded border-none px-3 py-1 my-1 text-sm font-semibold">Sign Up</button>
         </div>
     </div>
 </div>
