f1629520a9e6950d4f62331f6825db3eae5eff0d
kioshiajaja <106659652+kioshiajaja@users.noreply.github.com>
2024-12-13 07:40:37 +0800

    added stuff in homepage and changed the logo headerLanding/headerUser

--- upwardApp/src/lib/user/headerUser.svelte
+++ upwardApp/src/lib/user/headerUser.svelte
@@ -17,7 +17,9 @@
 
 <div class="sticky top-0 z-40 h-20 w-full p-5 mx-auto bg-secondary-800 shadow-xl">
     <div class="flex justify-between mx-20 items-center text-tertiary-300">
-        <span class="font-semibold max-sm:font-bold text-xl max-md:text-base max-[480px]:text-xs">UPward</span>
+        <a href="/" class="flex items-center">
+            <img src="/src/lib/landing/Upward logo1.png" alt="UPward Logo" class="h-10 max-sm:h-8 max-md:h-6 max-[480px]:h-4">
+        </a>
         <div class="flex items-center gap-3">
             <a href="/" class="btn bg-surface-50 hover:text-secondary-800 hover:bg-surface-300 rounded border-none px-3 py-1 my-1 text-sm font-semibold">Log Out</a>
         </div>
